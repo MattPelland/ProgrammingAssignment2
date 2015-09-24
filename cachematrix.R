1fc60f0b5f06717479c6a1f5c978636a54bd634a
MattPelland <MattPelland@users.noreply.github.com>
2015-09-23 22:05:08 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,12 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        ans <- NULL
+        set <- function(y){
+                 x <<- y
+                 m <<- NULL
+        }
+        
 }
 
 
